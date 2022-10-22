0f9d6420441cd843d4198f23153ea4d2b66fcec2
Savely <savely.domnikov@yandex.ru>
2022-10-22 23:22:48 +0300

    Created social website -> created app account

---
+++ account/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, HttpResponse
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login
+
+
+# Create your views here.
+
+
+def user_login(request):
+    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    return render(request, 'account/login.html', context={'form': form})
