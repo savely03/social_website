e580e90f6ddf7cac5ef596c920c29b6f25a78238
Savely <savely.domnikov@yandex.ru>
2022-10-23 23:47:59 +0300

    Completed the first version of the authentication system

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView, logout_then_login, PasswordChangeView, \
-    PasswordChangeDoneView
+    PasswordChangeDoneView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, \
+    PasswordResetCompleteView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
@@ -9,5 +10,10 @@ urlpatterns = [
     path('logout-then-login/', logout_then_login, name='logout-then-login'),
     path('password-change/', PasswordChangeView.as_view(), name='password_change'),
     path('password-change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    re_path(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', PasswordResetConfirmView.as_view(),
+            name='password_reset_confirm'),
+    path('password-reset/complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('', views.dashboard, name='dashboard'),
 ]
--- social_website/settings.py
+++ social_website/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 from django.urls import reverse_lazy
 from pathlib import Path
 
@@ -113,5 +114,10 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_REDIRECT_URL = reverse_lazy('dashboard')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 LOGIN_URL = reverse_lazy('login')
+LOGOUT_URL = reverse_lazy('logout')
+LOGIN_REDIRECT_URL = reverse_lazy('dashboard')
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
